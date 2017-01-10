2f0f4160b541dbe62de17599abceba1c98448637
BaranovaElizaveta <baranovaelizaveta@yandex.ru>
2017-01-10 18:49:22 +0600

    Client registration and authorization

---
+++ Reg&Autho_Monopoly/app/src/main/java/com/icerockdev/icedroid/signinexample/activities/RegistrationActivity.java
@@ -0,0 +1,149 @@
+package com.icerockdev.icedroid.signinexample.activities;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.icerockdev.icedroid.signinexample.R;
+import com.icerockdev.icedroid.signinexample.utils.NetworkProtocol;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private TextInputLayout mEmailLayout;
+    private TextInputLayout mLoginLayout;
+    private TextInputLayout mPasswordLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mEmailLayout = (TextInputLayout) findViewById(R.id.email_reg_layout);
+        mLoginLayout = (TextInputLayout) findViewById(R.id.login_reg_layout);
+        mPasswordLayout = (TextInputLayout) findViewById(R.id.password_reg_layout);
+        Button registrationBtn = (Button) findViewById(R.id.registration_btn);
+        registrationBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.registration_btn: {
+                if (areFieldsValid()) {
+                    String message = NetworkProtocol.getInstance().registrateUser(mLoginLayout.getEditText().getText().toString(),
+                            mEmailLayout.getEditText().getText().toString(),
+                            mPasswordLayout.getEditText().getText().toString());
+                    (new RegNewUserTask()).execute(message);
+                }
+                else {
+                    setError();
+                }
+                break;
+            }
+        }
+    }
+
+    private void onRegisteredCompleted(boolean status)
+    {
+        if (status) {
+            (Toast.makeText(getApplicationContext(), "Registration complete", Toast.LENGTH_LONG)).show();
+            finish();
+        } else {
+            setError();
+        }
+    }
+
+    //	It checks the email field
+    private boolean areFieldsValid()
+    {
+        return (mEmailLayout != null &&
+                mEmailLayout.getEditText() != null &&
+                !TextUtils.isEmpty(mEmailLayout.getEditText().getText()) &&
+                Patterns.EMAIL_ADDRESS.matcher(mEmailLayout.getEditText().getText()).matches()) &&
+                (mLoginLayout != null &&
+                        mLoginLayout.getEditText() != null &&
+                        !TextUtils.isEmpty(mLoginLayout.getEditText().getText())) &&
+                (mPasswordLayout != null &&
+                        mPasswordLayout.getEditText() != null &&
+                        !TextUtils.isEmpty(mPasswordLayout.getEditText().getText()));
+    }
+
+    private void setError()
+    {
+        if (mEmailLayout != null) {
+            mEmailLayout.setError(getString(R.string.email_is_not_valid_error_message));
+        }
+    }
+
+    private class RegNewUserTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... message) {
+            String response = null;
+
+            try {
+                Socket client = new Socket(NetworkProtocol.SERVER_IP_V4, NetworkProtocol.SERVER_PORT);
+                client.setSoTimeout(5 * 1000);
+                InputStream in = new BufferedInputStream(client.getInputStream());
+                BufferedOutputStream out = new BufferedOutputStream(client.getOutputStream());
+                out.write(message[0].getBytes("UTF-8"), 0, message[0].length());
+                out.flush();
+                byte[] buffer = new byte[1000];
+                int size = in.read(buffer);
+                if (size <= 0) {
+                    publishProgress("Network error");
+                } else {
+                    response = new String(buffer, 0, size, "UTF-8");
+                }
+            } catch (IOException e) {
+                publishProgress("Network error");
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... progress) {
+            (Toast.makeText(getApplicationContext(), progress[0], Toast.LENGTH_SHORT)).show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            boolean status = false;
+            try {
+                if (result != null) {
+                    JSONObject js = new JSONObject(result);
+                    if (js.getInt("Type") == 0) {
+                        status = js.getBoolean("Status");
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                status = false;
+            }
+            onRegisteredCompleted(status);
+        }
+
+    }
+}
+
---
+++ Reg&Autho_Monopoly/app/src/main/java/com/icerockdev/icedroid/signinexample/utils/AuthorizationUtils.java
@@ -0,0 +1,54 @@
+package com.icerockdev.icedroid.signinexample.utils;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AuthorizationUtils
+{
+	private static final String PREFERENCES_AUTHORIZED_KEY = "isAuthorized";
+	private static final String LOGIN_PREFERENCES = "LoginData";
+
+	/**
+	 * This method makes the user authorized
+	 *
+	 * @param context current context
+	 */
+
+	public static void setAuthorized(Context context, String login)
+	{
+
+		context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE)
+				.edit()
+				.putString(PREFERENCES_AUTHORIZED_KEY, login)
+				.apply();
+	}
+
+	/**
+	 * This method makes the user unauthorized
+	 *
+	 * @param context current context
+	 */
+
+	public static void logout(Context context)
+	{
+		context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE)
+				.edit()
+				.remove(PREFERENCES_AUTHORIZED_KEY)
+				.apply();
+	}
+
+	/**
+	 * This method checks if the user is authorized
+	 *
+	 * @param context current context
+	 * @return {@code true} if the user is authorized and {@code false} if not
+	 */
+
+	public static boolean isAuthorized(Context context)
+	{
+		SharedPreferences sp = context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE);
+		String login = sp.getString(PREFERENCES_AUTHORIZED_KEY, "");
+		return !login.isEmpty();
+	}
+}
