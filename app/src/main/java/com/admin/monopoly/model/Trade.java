e21a02901e058e0a79b68228d2bef17fefd0718a
author <author@mail.ru>
2017-01-14 23:45:22 +0700

    Забыл

---
+++ app/src/main/java/com/admin/monopoly/model/Trade.java
@@ -0,0 +1,17 @@
+
+package com.admin.monopoly.model;
+
+public class Trade {
+    public String salesman;
+    public String customer;
+    public int price;
+    public int cell;
+
+    public Trade(String salesman, String customer, int price, int cell) {
+        this.salesman = salesman;
+        this.customer = customer;
+        this.price = price;
+        this.cell = cell;
+    }
+    
+}
